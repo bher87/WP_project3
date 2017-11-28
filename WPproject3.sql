3f4abaa64ea97e22616537579c765eba50bcccf2
hanankwok532 <hanankwok532@gmail.com>
2017-11-28 02:21:16 -0500

    Upadted the statements I used to make the db tables

--- WPproject3.sql
+++ WPproject3.sql
@@ -4,6 +4,7 @@ USE project3;
 
 CREATE TABLE user (
 	id int(11) NOT NULL,
+	role varchar(50) NOT NULL,
 	username varchar(50) NOT NULL,
 	pword varchar(50) NOT NULL,
     firstname varchar(50) NOT NULL,
@@ -11,15 +12,8 @@ CREATE TABLE user (
     billinginfo varchar(50) NOT NULL
 )
 
-CREATE TABLE admin (
-	id int(11) NOT NULL,
-	adminName varchar(50) NOT NULL,
-	pword varchar(50) NOT NULL,
-	role varchar(50) NOT NULL
-)
-
-INSERT INTO admin (id, adminName, pword, role) VALUES
-(1, 'admin', 'admin', 'Admin')
+INSERT INTO user (id, role, username, pword, firstname, lastname, billinginfo) VALUES
+(1, 'Admin', 'aluna3', 'test532', 'Alex', 'Luna', '1234567890')
 
 CREATE TABLE inventory (
 	id int(11) NOT NULL,
